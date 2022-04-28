4d0eeb2ce2e0b327b32ef26351ab496051dd8204
강동훈 <gotoarmy@naver.com>
2022-04-28 12:09:44 +0900

    가위 바위 보

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,54 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
+
+
+
 public class Main {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         int n = in.nextInt();
-        ArrayList<Integer> inte = new ArrayList<Integer>();
+        ArrayList<Integer> A = new ArrayList<Integer>();
+        ArrayList<Integer> B = new ArrayList<Integer>();
         //
         //
         int index =0;
-        while(index < n)
+        while(index < n*2)
         {
-            inte.add(in.nextInt());
-            index++;
+             if(index <n)
+                 A.add(in.nextInt());
+             else
+                 B.add(in.nextInt());
+             index++;
         }
-        //자신의 앞수보다 작으면  출력
-        int visiblecount =0;
-        index =0;
-        int prev_num =0;
-        for (int z :inte)
-        {
-            if(prev_num < z) {
-                visiblecount++;
-                prev_num=z;
-            }
+
+        index=0;
+       for (int z : A)
+       {
+           int B_STATUS = B.get(index);
+           if(z==1)//가위
+           {
+               if(1==B_STATUS) System.out.println("D");
+               else if(2==B_STATUS) System.out.println("B");
+               else System.out.println("A");
+           }
+           if(z==2)//바위
+           {
+               if(2==B_STATUS) System.out.println("D");
+               else if(3==B_STATUS) System.out.println("B");
+               else System.out.println("A");
+
+           }
+           if(z==3)//보
+           {
+               if(3==B_STATUS) System.out.println("D");
+               else if(1==B_STATUS) System.out.println("B");
+               else System.out.println("A");
+           }
+           index++;
+       }
 
 
-        }
-        System.out.print(visiblecount);
-
-        return ;
     }
 }
