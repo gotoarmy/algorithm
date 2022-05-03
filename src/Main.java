932c785b10251c3a55639f0db601e5994bf5a078
강동훈 <gotoarmy@naver.com>
2022-05-03 14:58:45 +0900

    산봉우리인지 확인하는 문제 is that peak?

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         int n = in.nextInt();
-        bingosum bin =new bingosum();
-        ArrayList<Integer> A = new ArrayList<>();
-        int index =0;
-        while(index <n*n)
+        peak4D peak=new peak4D();
+        int[][] table =new int[n+2][n+2];
+        for (int i=0; i<n+2; i++)
         {
-            A.add(in.nextInt());
-            index++;
+            for (int j =0; j<n+2; j++)
+            {
+              if(0==i || 0==j ||  n+1 ==i || n+1 ==j)
+              {
+                  table[i][j] =0;
+              }
+              else
+              {
+                  table[i][j] =in.nextInt();
+              }
+            }
         }
-        bin.solu(n,A);
+
+        peak.solu(n,table);
     }
 }
