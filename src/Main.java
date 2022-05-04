fde41f073bcc6c08b780e654b9a92c1717c25982
bruce <gotoarmy@naver.com>
2022-05-04 18:20:20 +0900

    임시반장뽑기 세로열에서 득표수를뽑아 반장으로

--- src/Main.java
+++ src/Main.java
@@ -9,23 +9,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         int n = in.nextInt();
-        peak4D peak=new peak4D();
-        int[][] table =new int[n+2][n+2];
-        for (int i=0; i<n+2; i++)
+        leader sixgrade =new leader();
+        int[][] table =new int[n][5];
+        for (int i=0; i<n; i++)
         {
-            for (int j =0; j<n+2; j++)
+            for (int j =0; j<5; j++)
             {
-                if(0==i || 0==j ||  n+1 ==i || n+1 ==j)
-                {
-                    table[i][j] =0;
-                }
-                else
-                {
+
                     table[i][j] =in.nextInt();
-                }
+
             }
         }
 
-        peak.solu(n,table);
+        sixgrade.solu(n,table);
     }
 }
