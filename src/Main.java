3990d1e0167445feab0d9185e53410a8764407f3
강동훈 <gotoarmy@naver.com>
2022-05-12 16:09:44 +0900

    해쉬로 학급회장뽑기

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,59 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        최대길이수열2 T =new 최대길이수열2();
-        Scanner kb = new Scanner(System.in);
-        int n=kb.nextInt();
-        int k=kb.nextInt();
-        int[] arr=new int[n];
-        for(int i=0; i<n; i++){
-            arr[i]=kb.nextInt();
+        Scanner in = new Scanner(System.in);
+        int student_num = in.nextInt();
+        해쉬[] arr=new 해쉬[5];
+        int sequence=0;
+        char alpha=65;
+        while(sequence<5) //후보등록 A B C D E
+        {
+            해쉬 some=new 해쉬();
+            arr[sequence] =some;
+            some.setKey(alpha++);
+            sequence++;
         }
-        System.out.print(T.solution(n, k, arr));
+        char[] vote_result=new char[student_num]; //표입력받기
+        String vote = in.next();
+        vote_result=vote.toCharArray();
+        for(char z : vote_result)// 표확인
+        {
+            sequence=0;
+            while(true)
+            {
+                if(z==arr[sequence].key)
+                {
+                    arr[sequence].counting();
+                    break;
+                }
+
+                sequence++;
+            }
+
+
+        }
+        //당첨자 확인
+        int maxcount=0;
+        int i=0;
+        while (i < arr.length)
+        {
+           if(maxcount<arr[i].getCount())
+           {
+              maxcount =arr[i].getCount();
+           }
+            i++;
+        }
+        int j=0;
+        while(j <arr.length)
+        {
+            if(maxcount==arr[j].getCount())
+                System.out.print(arr[j].getKey());
+
+            j++;
+
+        }
+
+
 
 
     }
