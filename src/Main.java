2459251d5db0eac298d3068147dde8388d72f36f
bruce <gotoarmy@naver.com>
2022-05-14 20:27:00 +0900

    매출액의종류 슬라이딩윈도우,투포인터,해쉬맵

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.HashMap;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -8,12 +7,18 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        //AbaAeCe
-        //baeeACA
-        아나그램 name =new 아나그램();
-        Scanner in = new Scanner(System.in);
-        String first= in.next();
-        String second= in.next();
-        name.solu(first,second);
+        Scanner in=new Scanner(System.in);
+        매출액의종류 매출표=new 매출액의종류();
+        int day=0;
+        int iConsecutive_day= 0;
+        day = in.nextInt();
+        iConsecutive_day =in.nextInt();
+        int[] 매출전표 =new int[day];
+        int i =0;
+        while(i<day)
+        {
+            매출전표[i++]= in.nextInt();
+        }
+        매출표.solu(매출전표,day,iConsecutive_day);
     }
 }
---
+++ src/areasumM.java
@@ -0,0 +1,31 @@
+class areasumM {
+    public void solu(int[] arr, int sum)
+    {
+     int p1 =0; //합의 첫번째 요소를 가리키는 포인터
+     int p2 =0; //합의 마지막 인덱스를 가리키는 포인터
+     int sumcount =0;
+     int temp=0;
+        while (p2 <arr.length)
+        {
+            if(temp == sum)
+            {
+                sumcount++;
+                temp +=arr[p2++];
+            }
+            else if(temp <sum)
+            {
+                temp +=arr[p2++];
+
+            }
+            else if(temp >sum)
+            {
+                temp -=arr[p1++];
+
+            }
+
+        }
+        if (sum == temp) sumcount++;
+
+        System.out.print(sumcount);
+    }
+}
