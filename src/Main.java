04fcf73544df450cd5170c1b9d1e77765e874c9a
bruce <gotoarmy@naver.com>
2022-05-15 21:18:33 +0900

    매출액의종류 슬라이딩윈도우,투포인터,해쉬맵

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,9 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
-        매출액의종류 매출표=new 매출액의종류();
-        int day=0;
-        int iConsecutive_day= 0;
-        day = in.nextInt();
-        iConsecutive_day =in.nextInt();
-        int[] 매출전표 =new int[day];
-        int i =0;
-        while(i<day)
-        {
-            매출전표[i++]= in.nextInt();
-        }
-        매출표.solu(매출전표,day,iConsecutive_day);
+        모든아나그램찾기 anagram =new 모든아나그램찾기();
+        String first= in.next();
+        String second =in.next();
+        anagram.solu(first,second);
     }
 }
