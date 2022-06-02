30b00be6cf55c02cd4aee7426b144eeced1f6a5b
강동훈 <gotoarmy@naver.com>
2022-06-02 23:41:04 +0900

    이진트리와 스택

--- src/재귀DFSBFS/이진트리DPS출력.java
+++ src/재귀DFSBFS/이진트리DPS출력.java
@@ -1,35 +1,51 @@
 package 재귀DFSBFS;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
+class node{
+    node lt;
+    node rt;
+    int value;
+
+    public node(int value) {
+
+        this.value = value;
+    }
+}
+class bitree{
+    node root;
+    int count=0;
+
+    public void print(node node){
+        if(null==node)
+        {
+            return;
+        }
+        else {
+            //전위 후위 중위 스택을 그려보자
+            print(node.lt);
+            System.out.print(node.value);
+            print(node.rt);
+        }
+    }
+}
 public class 이진트리DPS출력 {
-    public void printprefix(int[]arr)
-    {
+
+    public static void main(String[] args) {
+        bitree bitree = new bitree();
+        bitree.root=new node(1);
+        bitree.root.lt=new node(2);
+        bitree.root.rt=new node(3);
+        bitree.root.lt.lt=new node(4);
+        bitree.root.lt.rt=new node(5);
+        bitree.root.rt.lt=new node(6);
+        bitree.root.rt.rt=new node(7);
+        이진트리DPS출력 이진트리DPS출력 = new 이진트리DPS출력();
+        이진트리DPS출력.solu(bitree);
     }
-    public void printmiddle(int[] arr)
-    {
-         boolean[] booleans = new boolean[arr.length];
-         Arrays.fill(booleans,false);
-
-    }
-    public void printpostfix(int[] arr){
-        boolean[] booleans = new boolean[arr.length];
-        Arrays.fill(booleans,false);
-
-    }
-    public boolean leftcheck(){
-        boolean answer=false;
-
-        return answer;
-    }
-    public  boolean rootcheck(){
-        boolean answer=false;
-        return answer;
-
-    }
-
-    public void solu(int[] arr) {
-
+    public void solu(bitree bit) {
+      bit.print(bit.root);
 
     }
 }
