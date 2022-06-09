e6a11d2bb20d9ba089dd4e441e31016103236ef6
강동훈 <gotoarmy@naver.com>
2022-06-10 02:01:59 +0900

    경로탐색구현중 남의코드를 참고하자

--- src/재귀DFSBFS/Main.java
+++ src/재귀DFSBFS/Main.java
@@ -11,15 +11,16 @@ public class Main {
         int n =kb.nextInt();
         int connectcount =kb.nextInt();
         graph graph = new graph();
-        //1.다섯개의 해더노드를 만든다.
-        for(int i=0;i<5; i++){
+        //1.다섯개의 해더와 노드를 만든다.
+        for(int i=0;i<n; i++){
             graph.addheader();
+            graph.addnode(i);
         }
-        //방향을 만들어준다,포인터가 몇개인지 어캐아냐? 런타임중에 생긴다.
+
         for(int j=0;j<connectcount; j++){
             int a =kb.nextInt();
             int b =kb.nextInt();
-            graph.connect(a,b);
+            graph.connect(a,b);//a
         }
         int i=0;
 
--- src/재귀DFSBFS/graph.java
+++ src/재귀DFSBFS/graph.java
@@ -1,21 +1,33 @@
 package 재귀DFSBFS;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Queue;
+
 class header{
-node head;
+ArrayList<Integer> possible=new ArrayList<>();
 }
 public class graph {
     int count=0;
     node head;
     LinkedList<header> list=new LinkedList<header>();
+    LinkedList<node> nodelist =new LinkedList<>();
 
 
-    public void connect(int value1, int value2) {
+    public void connect(int value1,int value2) {
+        header header = list.get(value1 - 1); //헤더를 뽑음
+        header.possible.add(value2);
 
-            }
+    }
 
     public void addheader() {
         header head = new header();
         list.add(head);
+
+    }
+    public void addnode(int i){
+        node node = new node(i + 1);
+        nodelist.add(node);
+        count++;
     }
 }
