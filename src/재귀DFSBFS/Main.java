da42179d515b4c05ec733e101e5ba643c1eee6d4
강동훈 <gotoarmy@naver.com>
2022-06-11 03:26:56 +0900

    경로탐색구현성공 ㅅㅂ 어떻게 했지..??

--- src/재귀DFSBFS/Main.java
+++ src/재귀DFSBFS/Main.java
@@ -10,7 +10,7 @@ public class Main {
         Scanner kb = new Scanner(System.in);
         int n =kb.nextInt();
         int connectcount =kb.nextInt();
-        graph graph = new graph();
+        graph graph = new graph(n);
         //1.다섯개의 해더와 노드를 만든다.
         for(int i=0;i<n; i++){
             graph.addheader();
@@ -20,8 +20,11 @@ public class Main {
         for(int j=0;j<connectcount; j++){
             int a =kb.nextInt();
             int b =kb.nextInt();
-            graph.connect(a,b);//a
+            graph.connect(a,b);//
         }
+        //1.1노드에서부터 5번까지 가는길을 구한다
+        graph.DFS(graph.list.get(0),n);
+        System.out.print(graph.answer);
         int i=0;
 
 
--- src/재귀DFSBFS/graph.java
+++ src/재귀DFSBFS/graph.java
@@ -1,6 +1,8 @@
 package 재귀DFSBFS;
 
+import java.nio.channels.InterruptedByTimeoutException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -8,10 +10,17 @@ class header{
 ArrayList<Integer> possible=new ArrayList<>();
 }
 public class graph {
+    int answer=0;
     int count=0;
     node head;
     LinkedList<header> list=new LinkedList<header>();
     LinkedList<node> nodelist =new LinkedList<>();
+    boolean[] visitied;
+
+    public graph(int n) {
+        visitied=new boolean[n];
+        Arrays.fill(visitied,false);
+    }
 
 
     public void connect(int value1,int value2) {
@@ -29,5 +38,33 @@ public class graph {
         node node = new node(i + 1);
         nodelist.add(node);
         count++;
+    }
+
+    public void solu(int n) {
+        //header
+        //0헤더를 훑는다 가능성배열을 추가한다
+
+
+    }
+    public void DFS(header header,int n)
+    {
+        header local=header;
+        visitied[0]=true;
+        for (int i : local.possible)
+        {
+            if(i == n){
+                answer++;
+                return;
+            }
+            else {
+                if(visitied[i-1]==false){
+                    visitied[i-1]=true;
+                    DFS(list.get(i-1),n);
+                    visitied[i-1]=false;
+                }
+
+            }
+        }
+
     }
 }
