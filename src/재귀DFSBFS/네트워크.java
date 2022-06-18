ec80ac864aff6ac9c0e80211373330b4cf2bbbc0
강동훈 <gotoarmy@naver.com>
2022-06-19 03:19:54 +0900

    프로그래머스3 네트워크 ㅜㅜ 드디어 풀었다 ㅠㅠ

--- src/재귀DFSBFS/네트워크.java
+++ src/재귀DFSBFS/네트워크.java
@@ -1,15 +1,14 @@
 package 재귀DFSBFS;
 
-import java.util.*;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class 네트워크{
     LinkedList<LinkedList<Integer>> list =new LinkedList<>(); //해더 like모음
-    LinkedList<LinkedList<Integer>> network=new LinkedList<>(); //연결된 네트워크
     int answer=0;
     boolean[] visiteda;
-    boolean[][] visited;
     public int solu(int n,int[][] computers) {
-        boolean[][] visited = new boolean[n][n];
         boolean[] visiteda =new boolean[n];
         for (int i=0; i<n; i++)
         {
@@ -26,26 +25,38 @@ public class 네트워크{
                   }
             }
 
-        answer= BFS(list);
+        for (int a =0;a<n;a++)
+        {
+            if(false==visiteda[a]){
+                BFS(a,list,visiteda);
+                visiteda[a]=true;
+            }
+
+        }
         return answer;
     }
-    public int BFS(LinkedList<LinkedList<Integer>> list){
-         //network를 조회해서 해당 노드가 있으면 노드건너뜀
-        visiteda=new boolean[list.size()];
-        Queue<Integer> name =new LinkedList<>();
-
-         for(int i=0; i<list.size(); i++)
-         {
-             LinkedList<Integer> temp = list.get(i);
-             //일단 1노드 네트워크를 만들고
-             //다음 노드가 1노드 네트워크에 포함되어있으면 다음노드로 넘어감
-             //포함되어있지 않은 노드일 경우 그 해당 노드 값들을 모두 네트워크에 포함되는지 확인
-             //안의 연결값들이 모두 포함되어있지 않다면 새로운 네트워크를 만들어서 저장
-             //모든 노드를 방문할때까지 반복
-         }
-        //네트워크의 계수를 리턴함
-        return network.size();
-
+    public void BFS(int startnode, LinkedList<LinkedList<Integer>> list, boolean[] visiteda){
+        boolean[] visiteda1 = visiteda;
+        Queue<Integer> queue =new LinkedList<>();
+        LinkedList<Integer> temp = list.get(startnode);
+        for (int a : temp)
+        {
+             queue.offer(a);
+        }
+        while(!queue.isEmpty()){
+            Integer poll = queue.poll();
+            visiteda[poll]=true;
+            LinkedList<Integer> temps = list.get(poll);
+            if(0==temps.size()) continue;
+            for(int h: temps)
+            {
+                if(false==visiteda[h])
+                {
+                    queue.offer(h);
+                }
+            }
+        }
+        answer++;
     }
 
 
